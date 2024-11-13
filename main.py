7b002bbbd8e7adc410ab1e9f0e27eb9d7b13bcd8
zikzz76 <zrahmadika@gmail.com>
2024-11-13 13:51:26 +0700

    menambahkan validasi untuk input panjang dan lebar

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 #main.py
 
+
+#fungsi untuk memeriksa apakah input adalah bilangan positif:
+def input_positif(prompt):
+    while True:
+        try:
+            nilai = float(input(prompt))
+            if nilai > 0:
+                return nilai 
+            else:
+                print("nilai harus lebih dari 0. coba lagi.")
+        except ValueError:
+            print("input tidak valid. masukkan angka.")
+                
+
 #meminta input panjang dan lebar dari pengguna
 panjang = float(input("Masukkan panjang persegi panjang:  "))
 lebar = float(input("Masukkan lebar persegi panjang:  "))
