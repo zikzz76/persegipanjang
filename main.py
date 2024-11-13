95a3eef5f36868b605891229b8025cfd7d7275fd
zikzz76 <zrahmadika@gmail.com>
2024-11-13 13:01:19 +0700

    menambahkan fungsi untuk menghitung luas persegi panjang

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+#main.py
 
+#meminta input panjang dan lebar dari pengguna
+panjang = float(input("Masukkan panjang persegi panjang:  "))
+lebar = float(input("Masukkan lebar persegi panjang:  "))
  
+ #menghitung luas
+ 
+luas = panjang * lebar
+
+#menampilkan hasil luas 
+print(f"luas persegi panjang adalah: {luas}")
